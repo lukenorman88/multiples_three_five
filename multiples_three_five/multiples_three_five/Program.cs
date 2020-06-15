b5ad4573ca1775deedfb8dc3465ca795318afabc
Luke <l.norman.dev@gmail.com>
2020-06-15 16:34:42 +0100

    Multiples of 3 and 5 upload

---
+++ multiples_three_five/multiples_three_five/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiples_three_five
+{
+    class Program
+    {               
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;                    
+                }
+            }
+
+            Console.WriteLine(result);
+        }    
+    }
+}
